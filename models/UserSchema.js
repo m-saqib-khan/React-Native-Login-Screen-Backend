9701864f382b0f7a8f41b1a48609e25a9285157d
zeeshan <zeeshankhanaptech@gmail.com>
2023-01-24 19:35:57 +0500

    resolve issues

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -16,4 +16,4 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-exports.default = mongoose.model("User",userSchema)
+module.exports = mongoose.model("User",userSchema)
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,29 +12,33 @@ router.post("/signup", async (req, res) => {
     const user = new User({ email, password, name });
     await user.save();
     const token = jwt.sign({ userId: user._id }, jwtkey);
-    res.send({ token });
+    res.send({succes:true,token});
   } catch (error) {
-    return res.status(422).send("ERROR");
+    return res.json({succes:false , msg:"ERROR",error});
   }
 });
 
 router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
+  try{
+
   if (!email || !password) {
-    return res.send({ error: "invalid email password" });
+    return res.send({ succes:false, error: "invalid email password" });
   }
   const user = await User.findOne({ email });
-  console.log(user.email, email, "user");
   if (!user) {
-    return res.send({ error: "invalid email password" });
+    return res.send({succes:false, error: "invalid email password" });
   }
   if (user.email === email && user.password === password) {
     console.log("asdasdasdaszxczxczx");
     const token = jwt.sign({ userId: user._id }, jwtkey);
-    res.send({ token });
+    res.send({succes:true, token });
   } else {
-    return res.send({ error: "invalid email password" });
+    return res.send({succes:false, error: "invalid email password" });
   }
+}catch(e){
+  res.json({succes:false,err:e})
+}
 });
 
 module.exports = router;
